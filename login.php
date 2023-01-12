9f010cfc9ef6c70690d5422aa20eb2809fe16448
Any97Cris <criscianysilva1997@gmail.com>
2023-01-12 09:36:42 -0400

    campos tela login

--- login.php
+++ login.php
@@ -13,9 +13,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-    <form method="POST">
-        
-    </form>
+    <div class="container"> 
+        <h3 class="mt-5">Tela de Acesso ao Usuário</h3>
+        <form method="POST" class="mt-5">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="nome@exemplo.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Senha</label>
+                <input type="password" class="form-control" name="senha" id="exampleFormControlInput1" placeholder="******">
+            </div>
+            <!-- position-absolute top-50 start-50 -->
+            <div class="d-grid gap-2 col-6 mx-auto mt-5">
+                <input class="btn btn-primary" type="submit" value="Entrar" />
+                <a href="#" class="btn btn-info">Cadastrar</a>
+            </div>
+            
+        </form>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
