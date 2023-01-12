f0910e35b6fbd16a617e0bbcb98a417d314ed719
Any97Cris <criscianysilva1997@gmail.com>
2023-01-12 09:00:28 -0400

    tela principal

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'database.php';
+
+session_start();
+
+if(!(isset($_SESSION['id']) && empty($_SESSION['id']) == false)){
+    header("Location: login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <h2>Bem vindo a página Inicial</h2>
+</body>
+</html>
